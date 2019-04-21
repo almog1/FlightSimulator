c4cc58d7485cfe788ab923ea8db63ed549feada9
almog gueta <almog.gueta@live.biu.ac.il>
2019-04-21 21:01:03 +0300

    auto pilot color changed

---
+++ FlightSimulator/ViewModels/AutoPilotViewModel.cs
@@ -0,0 +1,110 @@
+﻿using FlightSimulator.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Input;
+
+namespace FlightSimulator.ViewModels
+{
+    class AutoPilotViewModel : BaseNotify
+    {
+        private AutoPilotModel autoPilotModel;
+        string text;
+        private bool ischange;
+ 
+        public bool IsChanged
+        {
+            get { return ischange; }
+            set
+            {
+                ischange = value;
+                NotifyPropertyChanged("IsChanged");
+            }
+        }
+
+
+        public AutoPilotViewModel()
+        {
+            this.autoPilotModel = AutoPilotModel.Instance;
+            IsChanged = false;
+            text = "";
+                //check
+        }
+
+        public string TextAutoPilot
+        {
+            get
+            {
+                return text;
+            }
+            set
+            {
+                //if nothing - isChange
+                text = value;
+                if(text == "")
+                {
+                    IsChanged = false;
+                }
+                else
+                {
+                    IsChanged = true;
+                }
+                NotifyPropertyChanged("TextAutoPilot");
+
+            }
+        }
+
+        public void setValues()
+        {
+            autoPilotModel.SetValues();
+        }
+        
+        //Commands for the settings and connect buttoms
+        #region Commands
+        #region OKCommand
+        private ICommand _okCommand;
+        public ICommand OkCommand
+        {
+            get
+            {
+                return _okCommand ?? (_okCommand = new CommandHandler(() => OKClick()));
+            }
+        }
+
+        private void OKClick()
+        {
+            //need to send the changed values
+            //!!!!!!!!!!!!!!!!!!!!!!
+            ///!!!!!!!todo!!!!!!
+            ///!!!!!!!!!!!!!!!!!!
+            ///!!!!!!!!!!!!!!!!!!!
+            autoPilotModel.SetValues();
+            IsChanged = false;
+        }
+        #endregion
+
+        #region Clear command
+        private ICommand _clearCommand;
+        public ICommand ClearCommand
+        {
+            get
+            {
+                return _clearCommand ?? (_clearCommand = new CommandHandler(() => OnClear()));
+            }
+        }
+        private void OnClear()
+        {
+            //clear the screen
+            TextAutoPilot = "";
+            IsChanged = false; //need to change it
+        }
+        #endregion
+        #endregion
+        
+
+    }
+    
+}
---
+++ FlightSimulator/Views/BackgroundConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace FlightSimulator.Views
+{
+    class BackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isChange = (bool)value;
+            return isChange ? Brushes.Red : Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
